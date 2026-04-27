8d0b98379ffd1ffaf5add01cc65f3859c8264998
Joy Mikhael <31731899+JoyMikhaelLeb@users.noreply.github.com>
2026-04-27 14:43:38 +0300

    updated

--- create_numerical_task.py
+++ create_numerical_task.py
@@ -17,7 +17,7 @@ from datetime import timedelta
 import random
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("spherical-list-284723-216944ab15f1.json")
+    cred = credentials.Certificate("key.json")
     default_app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
